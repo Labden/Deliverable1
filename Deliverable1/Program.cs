ed68dd89ba5ea020de3e2fcdcd7205997803edfe
Trent Nedbal <Nedbal.trent@gmail.com>
2021-05-10 22:28:53 -0400

    deliverable1

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -12,10 +12,12 @@ namespace Deliverable1
 
             do
             {
-                Console.WriteLine("Would you like to get the measurement conversion for Milhip in inches or feet?");
+                Console.WriteLine("Would you like to get the measurement conversion for Milhip in inches, feet, fidget spinners, or memes?");
 
                 Console.WriteLine("(1)Inches");
                 Console.WriteLine("(2)Feet");
+                Console.WriteLine("(3)Fidget Spinners");
+                Console.WriteLine("(4)Memes");
                 int measurementType = Int32.Parse(Console.ReadLine());
 
                 if (measurementType == 1)
@@ -36,10 +38,27 @@ namespace Deliverable1
 
                     Console.WriteLine($"{memesConversion} memes");
                 }
+                else if (measurementType == 3)
+                {
+                    Console.WriteLine("Let's convert fidget spinners to inches! Enter an amount of fidget spinners:");
+                    decimal userFidgetSpinners = Decimal.Parse(Console.ReadLine());
+
+                    decimal inchesConversion = userFidgetSpinners * 1 / 3.5m;
+
+                    Console.WriteLine($"{inchesConversion} inches");
+                }
+                else if (measurementType == 4)
+                {
+                    Console.WriteLine("Let's convert memes to feet! Enter an amount of memes:");
+                    decimal userMemes = decimal.Parse(Console.ReadLine());
+
+                    decimal feetConversion = userMemes * .2m;
+
+                    Console.WriteLine($"{feetConversion} feet");
+                }
 
                 Console.WriteLine("Do you want to continue?");
                 userWantsToContinue = Console.ReadLine().ToUpper();
-
             } while (userWantsToContinue == "Y");
         }
     }
